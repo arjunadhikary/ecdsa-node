fa16b219d6bb6ac4c10a5711446094896f38b778
Arjun Adhikari <reasonworld12345@gmail.com>
2022-12-01 16:26:11 +0545

    feat: :fire: generate wallet with random adress and amount
    generate wallet with random adress and amount  and also random amount

---
+++ server/script/generatePrivatePublicKey.js
@@ -0,0 +1,22 @@
+const { utils, getPublicKey } = require('ethereum-cryptography/secp256k1');
+const { toHex } = require('ethereum-cryptography/utils');
+const fs = require('fs');
+const generateAddress = (limit) => {
+  let address = {};
+  for (let index = 0; index < limit; index++) {
+    const privateKey = utils.randomPrivateKey();
+    const publicKey = getPublicKey(privateKey);
+    const walletAddress = toHex(publicKey.slice(1).slice(-20));
+    console.log(`public key  : ${toHex(publicKey)}`);
+    console.log(`private key : ${toHex(privateKey)}`);
+    console.log(`address     : ${walletAddress}\n`);
+    address[walletAddress] = Math.floor(Math.random() * 100);
+  }
+  return address;
+};
+
+async function storeAddressInFile() {
+  fs.writeFileSync('address.json', JSON.stringify(generateAddress(3)), 'utf-8');
+}
+
+storeAddressInFile();
